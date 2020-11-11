90acffff821d41b6d6744022ee17549573fcd717
raeyoungii <skepo@naver.com>
2020-11-12 05:24:29 +0900

    data parse

--- app/src/main/java/com/UARTLoopback/UARTLoopbackActivity.java
+++ app/src/main/java/com/UARTLoopback/UARTLoopbackActivity.java
@@ -1,9 +1,7 @@
 package com.UARTLoopback;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -11,286 +9,339 @@ import android.os.Handler;
 import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.UARTLoopback.R.drawable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class UARTLoopbackActivity extends Activity {
 
-	// menu item
-	Menu myMenu;
-    final int MENU_CLEAN = Menu.FIRST;
+    // menu item
+    Menu myMenu;
+    final int MENU_CONFIGURE = Menu.FIRST;
+    final int MENU_CLEAN = Menu.FIRST + 1;
 
-	final int FORMAT_HEX = 0;
-	
-	int inputFormat = FORMAT_HEX;
+    final int FORMAT_HEX = 0;
 
-	StringBuffer readSB = new StringBuffer();
-    
-	/* thread to read the data */
-	public handler_thread handlerThread;
+    int inputFormat = FORMAT_HEX;
 
-	/* declare a FT311 UART interface variable */
-	public FT311UARTInterface uartInterface;
+    StringBuffer readSB = new StringBuffer();
 
-	/* graphical objects */
-	EditText readText;
+    /* thread to read the data */
+    public handler_thread handlerThread;
 
-	/* local variables */
-	byte[] writeBuffer;
-	byte[] readBuffer;
-	char[] readBufferToChar;
-	int[] actualNumBytes;
+    /* declare a FT312 UART interface variable */
+    public FT311UARTInterface uartInterface;
 
-	int numBytes;
-	byte count;
-	byte status;
-	byte writeIndex = 0;
-	byte readIndex = 0;
+    /* graphical objects */
+    EditText readText;
 
-	int baudRate; /* baud rate */
-	byte stopBit; /* 1:1stop bits, 2:2 stop bits */
-	byte dataBit; /* 8:8bit, 7: 7bit */
-	byte parity; /* 0: none, 1: odd, 2: even, 3: mark, 4: space */
-	byte flowControl; /* 0:none, 1: flow control(CTS,RTS) */
-	public Context global_context;
-	public boolean bConfiged = false;
-	public SharedPreferences sharePrefSettings;
-	Drawable originalDrawable;
-	public String act_string; 
+    /* local variables */
+    byte[] writeBuffer;
+    byte[] readBuffer;
+    char[] readBufferToChar;
+    int[] actualNumBytes;
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		sharePrefSettings = getSharedPreferences("UARTLBPref", 0);
-		//cleanPreference();
-		/* create editable text objects */
-		readText = (EditText) findViewById(R.id.ReadValues);
+    int numBytes;
+    byte count;
+    byte status;
+    byte writeIndex = 0;
+    byte readIndex = 0;
 
-		global_context = this;
+    int baudRate; /* baud rate */
+    byte stopBit; /* 1:1stop bits, 2:2 stop bits */
+    byte dataBit; /* 8:8bit, 7: 7bit */
+    byte parity; /* 0: none, 1: odd, 2: even, 3: mark, 4: space */
+    byte flowControl; /* 0:none, 1: flow control(CTS,RTS) */
+    public Context global_context;
+    public boolean bConfiged = false;
+    public SharedPreferences sharePrefSettings;
+    Drawable originalDrawable;
+    public String act_string;
 
-		/* allocate buffer */
-		writeBuffer = new byte[64];
-		readBuffer = new byte[4096];
-		readBufferToChar = new char[4096]; 
-		actualNumBytes = new int[1];
+    /* sensor Data */
+    int Gateway;
+    int Temperature;
+    int Humidity;
+    int Bio;
+    int P_btn;
+    int PIR;
+    int Door;
+    int Fire;
+    boolean Cancel = false;
 
-		/* setup the baud rate list */
-		baudRate = 115200;
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        sharePrefSettings = getSharedPreferences("UARTLBPref", 0);
+        //cleanPreference();
+        /* create editable text objects */
+        readText = (EditText) findViewById(R.id.ReadValues);
 
-		/* stop bits */
-		stopBit = 1;
+        global_context = this;
 
-		/* data bits */
-		dataBit = 8;
+        /* allocate buffer */
+        writeBuffer = new byte[64];
+        readBuffer = new byte[4096];
+        readBufferToChar = new char[4096];
+        actualNumBytes = new int[1];
 
-		/* parity */
-		parity = 0;
+        /* setup the baud rate list */
+        baudRate = 115200;
 
-		/* flow control */
-		flowControl = 0;
+        /* stop bits */
+        stopBit = 1;
 
-		act_string = getIntent().getAction();
-		if( -1 != act_string.indexOf("android.intent.action.MAIN")){
-			restorePreference();
-		}			
-		else if( -1 != act_string.indexOf("android.hardware.usb.action.USB_ACCESSORY_ATTACHED")){
-			cleanPreference();
-		}
+        /* data bits */
+        dataBit = 8;
 
-		uartInterface = new FT311UARTInterface(this, sharePrefSettings);
+        /* parity */
+        parity = 0;
 
-		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-		
-		handlerThread = new handler_thread(handler);
-		handlerThread.start();
-	}
-	
-	protected void cleanPreference(){
-		SharedPreferences.Editor editor = sharePrefSettings.edit();
-		editor.remove("configed");
-		editor.remove("baudRate");
-		editor.remove("stopBit");
-		editor.remove("dataBit");
-		editor.remove("parity");
-		editor.remove("flowControl");
-		editor.commit();
-	}
+        /* flow control */
+        flowControl = 0;
 
-	protected void savePreference() {
-		if(true == bConfiged){
-			sharePrefSettings.edit().putString("configed", "TRUE").commit();
-			sharePrefSettings.edit().putInt("baudRate", baudRate).commit();
-			sharePrefSettings.edit().putInt("stopBit", stopBit).commit();
-			sharePrefSettings.edit().putInt("dataBit", dataBit).commit();
-			sharePrefSettings.edit().putInt("parity", parity).commit();			
-			sharePrefSettings.edit().putInt("flowControl", flowControl).commit();			
-		}
-		else{
-			sharePrefSettings.edit().putString("configed", "FALSE").commit();
-		}
-	}
-	
-	protected void restorePreference() {
-		String key_name = sharePrefSettings.getString("configed", "");
-		if(true == key_name.contains("TRUE")){
-			bConfiged = true;
-		}
-		else{
-			bConfiged = false;
+        act_string = getIntent().getAction();
+        if (-1 != act_string.indexOf("android.intent.action.MAIN")) {
+            restorePreference();
+        } else if (-1 != act_string.indexOf("android.hardware.usb.action.USB_ACCESSORY_ATTACHED")) {
+            cleanPreference();
         }
 
-		baudRate = sharePrefSettings.getInt("baudRate", 115200);
-		stopBit = (byte)sharePrefSettings.getInt("stopBit", 1);
-		dataBit = (byte)sharePrefSettings.getInt("dataBit", 8);
-		parity = (byte)sharePrefSettings.getInt("parity", 0);
-		flowControl = (byte)sharePrefSettings.getInt("flowControl", 0);
-	}
-	
+        uartInterface = new FT311UARTInterface(this, sharePrefSettings);
 
-	//@Override
-	public void onHomePressed() {
-		onBackPressed();
-	}	
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
-	public void onBackPressed() {
-	    super.onBackPressed();
-	}	
-	
-	@Override
-	protected void onResume() {
-		// Ideally should implement onResume() and onPause()
-		// to take appropriate action when the activity looses focus
-		super.onResume();		
-		if( 2 == uartInterface.ResumeAccessory() )
-		{
-			cleanPreference();
-			restorePreference();
-		}
-	}
+        handlerThread = new handler_thread(handler);
+        handlerThread.start();
+    }
 
-	@Override
-	protected void onPause() {
-		// Ideally should implement onResume() and onPause()
-		// to take appropriate action when the activity looses focus
-		super.onPause();
-	}
+    protected void cleanPreference() {
+        SharedPreferences.Editor editor = sharePrefSettings.edit();
+        editor.remove("configed");
+        editor.remove("baudRate");
+        editor.remove("stopBit");
+        editor.remove("dataBit");
+        editor.remove("parity");
+        editor.remove("flowControl");
+        editor.commit();
+    }
 
-	@Override
-	protected void onStop() {
-		// Ideally should implement onResume() and onPause()
-		// to take appropriate action when the activity looses focus
-		super.onStop();
-	}
+    protected void savePreference() {
+        if (true == bConfiged) {
+            sharePrefSettings.edit().putString("configed", "TRUE").commit();
+            sharePrefSettings.edit().putInt("baudRate", baudRate).commit();
+            sharePrefSettings.edit().putInt("stopBit", stopBit).commit();
+            sharePrefSettings.edit().putInt("dataBit", dataBit).commit();
+            sharePrefSettings.edit().putInt("parity", parity).commit();
+            sharePrefSettings.edit().putInt("flowControl", flowControl).commit();
+        } else {
+            sharePrefSettings.edit().putString("configed", "FALSE").commit();
+        }
+    }
 
-	@Override
-	protected void onDestroy() {
-		uartInterface.DestroyAccessory(bConfiged);
-		super.onDestroy();
-	}
+    protected void restorePreference() {
+        String key_name = sharePrefSettings.getString("configed", "");
+        if (true == key_name.contains("TRUE")) {
+            bConfiged = true;
+        } else {
+            bConfiged = false;
+        }
+
+        baudRate = sharePrefSettings.getInt("baudRate", 115200);
+        stopBit = (byte) sharePrefSettings.getInt("stopBit", 1);
+        dataBit = (byte) sharePrefSettings.getInt("dataBit", 8);
+        parity = (byte) sharePrefSettings.getInt("parity", 0);
+        flowControl = (byte) sharePrefSettings.getInt("flowControl", 0);
+    }
 
 
-	final Handler handler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			
-			for(int i=0; i<actualNumBytes[0]; i++)
-			{
-				readBufferToChar[i] = (char)readBuffer[i];
-			}
-			appendData(readBufferToChar, actualNumBytes[0]);
-		}
-	};
+    //@Override
+    public void onHomePressed() {
+        onBackPressed();
+    }
 
-	/* usb input data handler */
-	private class handler_thread extends Thread {
-		Handler mHandler;
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 
-		/* constructor */
-		handler_thread(Handler h) {
-			mHandler = h;
-		}
+    @Override
+    protected void onResume() {
+        // Ideally should implement onResume() and onPause()
+        // to take appropriate action when the activity looses focus
+        super.onResume();
+        if (2 == uartInterface.ResumeAccessory()) {
+            cleanPreference();
+            restorePreference();
+        }
+    }
 
-		public void run() {
-			Message msg;
+    @Override
+    protected void onPause() {
+        // Ideally should implement onResume() and onPause()
+        // to take appropriate action when the activity looses focus
+        super.onPause();
+    }
 
-			while (true) {
-				
-				try {
-					Thread.sleep(200);
-				} catch (InterruptedException e) {
-				}
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 
-				status = uartInterface.ReadData(4096, readBuffer,actualNumBytes);
+    @Override
+    protected void onDestroy() {
+        uartInterface.DestroyAccessory(bConfiged);
+        super.onDestroy();
+    }
 
-				if (status == 0x00 && actualNumBytes[0] > 0) {
-					msg = mHandler.obtainMessage();
-					mHandler.sendMessage(msg);
-				}
 
-			}
-		}
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		myMenu = menu;
-		myMenu.add(0, MENU_CLEAN, 0, "Clean Read Bytes Field");		
-		return super.onCreateOptionsMenu(myMenu);
-	}	
+    final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            for (int i = 0; i < actualNumBytes[0]; i++) {
+                readBufferToChar[i] = (char) readBuffer[i];
+            }
+            appendData(readBufferToChar, actualNumBytes[0]);
+        }
+    };
+
+    /* usb input data handler */
+    private class handler_thread extends Thread {
+        Handler mHandler;
+
+        /* constructor */
+        handler_thread(Handler h) {
+            mHandler = h;
+        }
+
+        public void run() {
+            Message msg;
+
+            while (true) {
+
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                }
+
+                status = uartInterface.ReadData(4096, readBuffer, actualNumBytes);
+
+                if (status == 0x00 && actualNumBytes[0] > 0) {
+                    msg = mHandler.obtainMessage();
+                    mHandler.sendMessage(msg);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        myMenu = menu;
+        myMenu.add(0, MENU_CONFIGURE, 0, "Configure");
+        myMenu.add(0, MENU_CLEAN, 0, "Clean Field");
+        return super.onCreateOptionsMenu(myMenu);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId())
-        {
-        case MENU_CLEAN:
-        default:        	
-        	readSB.delete(0, readSB.length());
-        	readText.setText(readSB);
-        	break;
+        switch (item.getItemId()) {
+            case MENU_CONFIGURE:
+                if (false == bConfiged) {
+                    bConfiged = true;
+                    uartInterface.SetConfig(baudRate, dataBit, stopBit, parity, flowControl);
+                    savePreference();
+                }
+                break;
+
+            case MENU_CLEAN:
+            default:
+                readSB.delete(0, readSB.length());
+                readText.setText(readSB);
+                break;
         }
- 
+
         return super.onOptionsItemSelected(item);
     }
 
-    
-    public void appendData(char[] data, int len)
-    {
-    	if(len >= 1)    		
-    		readSB.append(String.copyValueOf(data, 0, len));
 
-		char[] ch = readSB.toString().toCharArray();
-		String temp;
-		StringBuilder tmpSB = new StringBuilder();
-		for(int i = 0; i < ch.length; i++)
-		{
-			temp = String.format("%02x", (int) ch[i]);
+    public void appendData(char[] data, int len) {
+        if (len >= 1) {
+            readSB.append(String.copyValueOf(data, 0, len));
+        }
 
-			if(temp.length() == 4)
-			{
-				tmpSB.append(temp.substring(2, 4));
-			}
-			else
-			{
-				tmpSB.append(temp);
-			}
+        char[] ch = readSB.toString().toCharArray();
+        String temp;
+        StringBuilder tmpSB = new StringBuilder();
+        String[] tmpArr = new String[ch.length];
 
-			if(i+1 < ch.length)
-			{
-				tmpSB.append(" ");
-			}
-		}
-		readText.setText(tmpSB);
-		tmpSB.delete(0, tmpSB.length());
+        for (int i = 0; i < ch.length; i++) {
+            temp = String.format("%02x", (int) ch[i]);
+
+            if (temp.length() == 4) {
+                tmpSB.append(temp.substring(2, 4));
+                tmpArr[i] = temp.substring(2, 4);
+            } else {
+                tmpSB.append(temp);
+                tmpArr[i] = temp;
+            }
+
+            if (i + 1 < ch.length) {
+                tmpSB.append(" ");
+            }
+        }
+
+        /* current time */
+        StringBuilder tmpSB2 = new StringBuilder();
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat mFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        String formatDate = mFormat.format(date) + '\n';
+
+        tmpSB2.append(formatDate);
+        tmpSB2.append(tmpSB);
+
+        // TODO: 데이터 파싱 후 DB에 삽입
+        /* check cmd */
+        String cmd = tmpArr[6];
+
+        if (true == cmd.equals("30")) {
+            Gateway = Integer.parseInt(tmpArr[13] + tmpArr[14], 16);
+            Temperature = Integer.parseInt(tmpArr[27] + tmpArr[28], 16);
+            Humidity = Integer.parseInt(tmpArr[41] + tmpArr[42], 16);
+            Bio = Integer.parseInt(tmpArr[55] + tmpArr[56], 16);
+            P_btn = Integer.parseInt(tmpArr[69] + tmpArr[70], 16);
+            PIR = Integer.parseInt(tmpArr[83] + tmpArr[84], 16);
+            Door = Integer.parseInt(tmpArr[97] + tmpArr[98], 16);
+            Fire = Integer.parseInt(tmpArr[111] + tmpArr[112], 16);
+
+            String sensorData = "\nGateway: " + Gateway +
+                    "\nTemperature: " + Temperature +
+                    "\tHumidity: " + Humidity +
+                    "\tBio: " + Bio +
+                    "\nP_btn: " + P_btn +
+                    "\tPIR: " + PIR +
+                    "\tDoor: " + Door +
+                    "\tFire: " + Fire;
+
+            tmpSB2.append(sensorData);
+        } else {
+            cmd = cmd.concat(tmpArr[7] + tmpArr[8]);
+
+            if (true == cmd.equals("611020")) {
+                Cancel = true;
+            }
+        }
+
+        readText.setText(tmpSB2);
+
+        readSB.delete(0, readSB.length());
+        tmpSB.delete(0, tmpSB.length());
+        tmpSB2.delete(0, tmpSB.length());
     }
 }
